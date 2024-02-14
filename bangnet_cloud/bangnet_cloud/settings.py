5ca35ac2a9f976f6699cfd9b570a3cf1d7fb6ea3
linweilong <linweilong@huya.com>
2024-02-14 11:29:47 +0000

    refine

--- bangnet_cloud/bangnet_cloud/settings.py
+++ bangnet_cloud/bangnet_cloud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-8(wxqq*ca*imt(da=+q!unot!=)1dy00$@xuyxzu1c@ylbl76@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['34.241.234.234']
 
 
 # Application definition
