bd083a9f65df6f2d4b2993b0f08af91bcb1f8f8f
Cian Mawhinney <cianmawhinney@gmail.com>
2024-02-18 19:25:35 +0000

    Update test server to generate random servo positions

--- test-server/index.js
+++ test-server/index.js
@@ -9,6 +9,21 @@ app.post('/', async (req, res) => {
   res.send('ok');
 })
 
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+app.get('/servo', (req, res) => {
+  let minServoAngle = 0;
+  let maxServoAngle = 180;
+  let servoAngle = getRandomInt(minServoAngle, maxServoAngle);
+  
+  res.status(200);
+  res.send(servoAngle.toString());
+})
+
 app.listen(port, '0.0.0.0', () => {
   console.log(`Example app listening on port ${port}`)
 })
