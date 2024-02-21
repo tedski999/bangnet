d88bf21e223c048f03ac87eef1438167a9420407
tedski999 <ski@h8c.de>
2024-02-21 17:55:22 +0000

    Clean up entire codebase

--- firmware/sensor/main/main.cpp
+++ firmware/sensor/main/main.cpp
@@ -1,3 +1,4 @@
+#include "button.hpp"
 #include "ntp.hpp"
 #include "oled.hpp"
 #include "ota.hpp"
@@ -10,33 +11,27 @@
 #define WIFI_SSID "Swifty House Wifi"
 #define WIFI_PSWD "Cheeseandham7"
 
-#define BUTTON_PIN 0
-#define BUTTON_TIME_MSEC 100
-
-static void on_timeout(TimerHandle_t timer)
-{
-	if (digitalRead(BUTTON_PIN) == 0)
-		bnet_oled_show();
-}
-
 extern "C" void app_main()
 {
 	ESP_LOGI(TAG, "Initialising board...");
 	initArduino();
 	randomSeed(analogRead(0));
+	bnet_button_start();
 
-	ESP_LOGI(TAG, "Configuring button handler...");
-	pinMode(BUTTON_PIN, INPUT_PULLUP);
-	TimerHandle_t timer = xTimerCreate("button_timer", BUTTON_TIME_MSEC / portTICK_PERIOD_MS, true, NULL, on_timeout);
-	xTimerStart(timer, 0);
-
+	// Try connect to AP for 10 seconds
 	ESP_LOGI(TAG, "Connecting to WiFi...");
 	WiFi.begin(WIFI_SSID, WIFI_PSWD);
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(100);
-	}
+	for (int i = 0; WiFi.status() != WL_CONNECTED && i < 10; i++)
+		delay(1 * MSECS_TO_SEC);
+	if (WiFi.status() != WL_CONNECTED)
+		WiFi.mode(WIFI_OFF);
 
-	ESP_LOGI(TAG, "Starting application systems...");
-	bnet_ntp_start();
-	bnet_ota_start();
+	// Start appropriate systems
+	if (WiFi.status() == WL_STOPPED) {
+		ESP_LOGI(TAG, "Unable to connect to WiFi, enabling peer-connection systems...");
+	} else {
+		ESP_LOGI(TAG, "Connected to WiFi, enabling direct-connection systems...");
+		bnet_ntp_start();
+		bnet_ota_start();
+	}
 }
--- firmware/sensor/main/ota.cpp
+++ firmware/sensor/main/ota.cpp
@@ -8,121 +8,132 @@
 
 #define TAG "ESP32resso OTA"
 
+#define TASK_STACK_SIZE (8096)
+#define TASK_PRIORITY (5)
+
+// NOTE: Run `python3 -m http.server` in /firmware/sensor directory
 #define HOST "192.168.0.179"
-#define PORT (80)
-#define MIN_POLL_INTERVAL_SECS (6)
-#define MAX_POLL_INTERVAL_SECS (12)
+#define PORT (8000)
+#define PATH "/build/sensor.bin"
 
-static void on_timeout(TimerHandle_t timer)
+#define MIN_DELAY_SECS (3600)
+#define MAX_DELAY_SECS (7200)
+#define ERR_DELAY_SECS (1800)
+
+// Query for firmware updates, update firmware if newer, else schedule the next query
+static void task(void *args)
 {
-	ESP_LOGI(TAG, "Checking for firmware updates...");
-
-	/*
-
-	const esp_app_desc_t *app_desc = esp_app_get_description();
-
-	esp_http_client_config_t http_config;
-	http_config.url = HOST;
-
-	esp_http_client_handle_t http_handle = esp_http_client_init(&http_config);
-	if (!http_handle) {
-		ESP_LOGE(TAG, "Unable to create HTTP client");
-		goto on_timeout_end0;
-	}
+	TimerHandle_t timer = (TimerHandle_t) args;
+	int delay_msec = ERR_DELAY_SECS * MSECS_TO_SEC;
 
 	esp_err_t err;
+	const esp_app_desc_t *app_desc;
+	esp_http_client_config_t http_config = {};
+	esp_http_client_handle_t http_handle;
+	const esp_partition_t *new_partition;
 	esp_ota_handle_t ota_handle;
+	int64_t content_length;
+	int status_code;
+	char read_buf[1024];
+	int read_len;
 
-	{
-		ESP_LOGI(TAG, "Querying for firmware...");
+	ESP_LOGI(TAG, "Checking for firmware updates...");
 
-		err = esp_http_client_open(http_handle, 0);
-		if (err != ESP_OK) {
-			ESP_LOGE(TAG, "Unable to open HTTP connection");
-			goto on_timeout_end1;
-		}
+	app_desc = esp_app_get_description();
 
-		int64_t content_length = esp_http_client_fetch_headers(http_handle);
-		if (content_length < 0) {
-			ESP_LOGE(TAG, "Unable to read HTTP headers");
-			goto on_timeout_end2;
-		}
-
-		int status_code = esp_http_client_get_status_code(http_handle);
-		if (status_code != 200) {
-			ESP_LOGE(TAG, "Received non-200 status code: %d", status_code);
-			goto on_timeout_end2;
-		}
-
-		ESP_LOGI(TAG, "Updating firmware...");
-		// TODO: query for new updates
-		// ESP_LOGI(TAG, "Updating to firmware version %s", new_app_ver);
-		// if (new_app_ver[0] <= app_ver[0]) {
-		// 	ESP_LOGI(TAG, "No new firmware update found");
-		// 	goto on_timeout_end0;
-		// }
-
-		const esp_partition_t *new_partition = esp_ota_get_next_update_partition(NULL);
-		if (new_partition) {
-			ESP_LOGI(TAG, "Unable to find valid OTA partition to use");
-			goto on_timeout_end2;
-		}
-
-		err = esp_ota_begin(new_partition, OTA_SIZE_UNKNOWN, &ota_handle);
-		if (err != ESP_OK) {
-			ESP_LOGI(TAG, "Unable to start OTA");
-			goto on_timeout_end2;
-		}
-
-		while (!esp_http_client_is_complete_data_received(http_handle)) {
-			char buf[1024];
-			int len = sizeof buf;
-			len = esp_http_client_read(http_handle, buf, len);
-			if (len < 0) {
-				ESP_LOGE(TAG, "Unable to read HTTP data");
-				goto on_timeout_end3;
-			}
-			err = esp_ota_write(ota_handle, buf, len);
-			if (err != ESP_OK) {
-				ESP_LOGE(TAG, "OTA write error");
-				goto on_timeout_end3;
-			}
-		}
-
-		err = esp_ota_end(ota_handle);
-		if (err != ESP_OK) {
-			ESP_LOGE(TAG, "OTA end error");
-		}
-
-		err = esp_ota_set_boot_partition(new_partition);
-		if (err != ESP_OK) {
-			ESP_LOGE(TAG, "OTA set boot partition error");
-		}
-
-		esp_restart();
+	http_config.host = HOST;
+	http_config.port = PORT;
+	http_config.path = PATH;
+	if (!(http_handle = esp_http_client_init(&http_config))) {
+		ESP_LOGE(TAG, "Unable to create HTTP client");
+		goto task_abort0;
 	}
 
-on_timeout_end3:
-	err = esp_ota_abort(ota_handle);
-	if (err != ESP_OK) {
+	if ((err = esp_http_client_open(http_handle, 0)) != ESP_OK) {
+		ESP_LOGE(TAG, "Unable to open HTTP connection");
+		goto task_abort1;
+	}
+
+	if ((content_length = esp_http_client_fetch_headers(http_handle)) < 0) {
+		ESP_LOGE(TAG, "Unable to read HTTP headers");
+		goto task_abort1;
+	}
+
+	if ((status_code = esp_http_client_get_status_code(http_handle)) != 200) {
+		ESP_LOGE(TAG, "Received non-200 status code: %d", status_code);
+		goto task_abort1;
+	}
+
+	if (true) { // TODO: check if version is newer than current
+		ESP_LOGI(TAG, "No new firmware update found");
+		delay_msec = random(MIN_DELAY_SECS * MSECS_TO_SEC, MAX_DELAY_SECS * MSECS_TO_SEC);
+		goto task_abort1;
+	}
+
+	ESP_LOGI(TAG, "Updating firmware...");
+
+	if (!(new_partition = esp_ota_get_next_update_partition(NULL))) {
+		ESP_LOGE(TAG, "Unable to find valid OTA partition to use");
+		goto task_abort1;
+	}
+
+	if ((err = esp_ota_begin(new_partition, OTA_SIZE_UNKNOWN, &ota_handle)) != ESP_OK) {
+		ESP_LOGE(TAG, "Unable to start OTA");
+		goto task_abort1;
+	}
+
+	// Write HTTP response chunk by chunk into new boot partition
+	// TODO: delta updates?
+	while (!esp_http_client_is_complete_data_received(http_handle)) {
+		if ((read_len = esp_http_client_read(http_handle, read_buf, sizeof read_buf)) < 0) {
+			ESP_LOGE(TAG, "Unable to read HTTP data");
+			goto task_abort2;
+		} else if ((err = esp_ota_write(ota_handle, read_buf, read_len)) != ESP_OK) {
+			ESP_LOGE(TAG, "OTA write error");
+			goto task_abort2;
+		}
+	}
+
+	if ((err = esp_ota_end(ota_handle)) != ESP_OK) {
+		ESP_LOGE(TAG, "OTA end error");
+		goto task_abort2;
+	}
+
+	if ((err = esp_ota_set_boot_partition(new_partition)) != ESP_OK) {
+		ESP_LOGE(TAG, "OTA set boot partition error");
+		goto task_abort2;
+	}
+
+	ESP_LOGI(TAG, "Rebooting into new partition...");
+	esp_restart();
+
+task_abort2:
+	if ((err = esp_ota_abort(ota_handle)) != ESP_OK)
 		ESP_LOGE(TAG, "OTA abort error");
-	}
-
-on_timeout_end2:
-	esp_http_client_close(http_handle);
-
-on_timeout_end1:
-	esp_http_client_cleanup(http_handle);
-
-on_timeout_end0:
-	long delay = random(MIN_POLL_INTERVAL_SECS * MSECS_TO_SEC, MAX_POLL_INTERVAL_SECS * MSECS_TO_SEC);
-	xTimerChangePeriod(timer, delay / portTICK_PERIOD_MS, 0);
-	xTimerReset(timer, 0);
-	ESP_LOGI(TAG, "Scheduled next query: %f secs", ((double) delay) / MSECS_TO_SEC);
-	*/
+task_abort1:
+	if ((err = esp_http_client_cleanup(http_handle)) != ESP_OK)
+		ESP_LOGE(TAG, "HTTP cleanup error");
+task_abort0:
+	ESP_LOGI(TAG, "Scheduled next query: %f secs", delay_msec / (double) MSECS_TO_SEC);
+	xTimerChangePeriod(timer, delay_msec / portTICK_PERIOD_MS, portMAX_DELAY);
+	xTimerReset(timer, portMAX_DELAY);
+	vTaskDelete(NULL);
 }
 
-void bnet_ota_start() {
-	TimerHandle_t timer = xTimerCreate("ota_timer", 1, true, NULL, on_timeout);
-	xTimerStart(timer, 0);
+// Perform the next OTA query
+static void on_timeout(TimerHandle_t timer)
+{
+	if (xTaskCreate(task, "ota task", TASK_STACK_SIZE, timer, TASK_PRIORITY, NULL) != pdPASS) {
+		ESP_LOGE(TAG, "Unable to create task, trying again in %d seconds...", ERR_DELAY_SECS);
+		xTimerChangePeriod(timer, (ERR_DELAY_SECS * MSECS_TO_SEC) / portTICK_PERIOD_MS, portMAX_DELAY);
+		xTimerReset(timer, portMAX_DELAY);
+	}
+}
+
+// Regularly query server for new firmware updates
+void bnet_ota_start()
+{
+	TimerHandle_t timer = xTimerCreate("ota_timer", 1, false, NULL, on_timeout);
+	if (!timer || xTimerStart(timer, portMAX_DELAY) != pdPASS)
+		ESP_LOGE(TAG, "Timer init error");
 }
