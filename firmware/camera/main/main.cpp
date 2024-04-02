47a4aa9de062cbe82d9266c519c81103f6fb535b
tedski999 <ski@h8c.de>
2024-04-02 22:06:04 +0100

    Add OTA to camera and servo firmwares

--- firmware/camera/main/main.cpp
+++ firmware/camera/main/main.cpp
@@ -1,5 +1,8 @@
 #include "Arduino.h"
 #include "esp_camera.h"
+#include <esp_log.h>
+#include <esp_ota_ops.h>
+#include <esp_http_client.h>
 #include <ArduinoHttpClient.h>
 #include <WiFi.h>
 
@@ -8,14 +11,22 @@
 #include <freertos/semphr.h>
 #include <freertos/timers.h>
 
+extern "C" {
+#include <detools.h>
+}
+
+#define CLOUD_HOST "34.241.234.234"
+#define CLOUD_PORT (8000)
+#define CLOUD_MIN_POLL_MSEC (30000)
+#define CLOUD_MAX_POLL_MSEC (60000)
+
 // FreeRTOS task parameters
 #define TASK_STACK_SIZE (8192)
 #define TASK_PRIORITY (5)
 
 // WiFi settings
-// TODO: make this more general or read these from another file
-const char* ssid = "Cian";
-const char* password = "12345678";
+#define WIFI_SSID "msungie"
+#define WIFI_PSWD "hamandcheese"
 
 // pinout for camera on ESP-EYE according to https://github.com/espressif/esp-who/blob/master/docs/en/Camera_connections.md
 #define PWDN_GPIO_NUM     -1
@@ -91,11 +102,13 @@ void setupCamera() {
 void setup() {
   Serial.begin(115200);
   Serial.print("Attempting to connect to network called: ");
-  Serial.println(ssid); 
-  WiFi.begin(ssid, password);
-  while ( WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(100); // wait 100ms
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PSWD);
+  for (int i = 0; WiFi.status() != WL_CONNECTED && i < 30; i++)
+    delay(1000);
+  if (WiFi.status() != WL_CONNECTED) {
+    ESP_LOGE("$MAIN", "Failed to connect to WiFi, rebooting...");
+    esp_restart();
   }
   Serial.println();
 
@@ -205,9 +218,119 @@ void task(void *arg) {
     }
 }
 
+// Occasionally poll cloud for delta-encoded over-the-air firmware updates
+static esp_ota_handle_t ota_handle;
+static const esp_partition_t *ota_new_partition, *ota_old_partition;
+static uintptr_t ota_seek_addr = 0;
+static int ota_read(void *arg, uint8_t *buf, size_t len) { return esp_partition_read(ota_old_partition, ota_seek_addr, buf, len); }
+static int ota_seek(void *arg, int offset) { ota_seek_addr += offset; return 0; }
+static int ota_write(void *arg, const uint8_t *buf, size_t len) { return esp_ota_write(ota_handle, buf, len) || ota_seek(arg, len); }
+static void ota_task(void *arg) {
+  printf("Start of firmware update task\n");
+  uint64_t version = * (uint64_t *) esp_app_get_description()->app_elf_sha256;
+  while (true) {
+    delay(random(CLOUD_MIN_POLL_MSEC, CLOUD_MAX_POLL_MSEC));
+
+  char path[256];
+  esp_err_t err;
+  esp_http_client_config_t http_config = {};
+  esp_http_client_handle_t http_handle;
+  int64_t content_len = 0, read_len = 0;
+  int status_code;
+  struct detools_apply_patch_t patch;
+
+  ESP_LOGD("$", "Querying OTA for version %llX...", version);
+  snprintf(path, sizeof path, "/update?version=camera.%llX", version);
+  http_config.host = CLOUD_HOST;
+  http_config.port = CLOUD_PORT;
+  http_config.path = path;
+  if (!(http_handle = esp_http_client_init(&http_config))) {
+    ESP_LOGE("$", "Unable to create HTTP client");
+    continue;
+  }
+  if ((err = esp_http_client_open(http_handle, 0)) != ESP_OK) {
+    ESP_LOGE("$", "Unable to open HTTP connection: %s (err %d)", esp_err_to_name(err), err);
+    goto end1;
+  }
+  if ((content_len = esp_http_client_fetch_headers(http_handle)) < 0) {
+    ESP_LOGE("$", "Unable to read HTTP headers");
+    goto end1;
+  }
+  if ((status_code = esp_http_client_get_status_code(http_handle)) != 200) {
+    ESP_LOGE("$", "Received non-200 status code: %d", status_code);
+    goto end1;
+  }
+  if (!content_len) {
+    ESP_LOGD("$", "No OTA for version %llX received", version);
+    goto end1;
+  }
+
+  ESP_LOGI("$", "Updating firmware (%lld byte patch)...", content_len);
+
+  ota_seek_addr = 0;
+  if (!(ota_new_partition = esp_ota_get_next_update_partition(NULL))) {
+    ESP_LOGE("$", "Unable to find valid OTA partition to use");
+    goto end1;
+  }
+  if (!(ota_old_partition = esp_ota_get_running_partition())) {
+    ESP_LOGE("$", "Unable to find current OTA partition in use");
+    goto end1;
+  }
+  err = esp_ota_begin(ota_new_partition, OTA_SIZE_UNKNOWN, &ota_handle);
+  if (err != ESP_OK) {
+    ESP_LOGE("$", "Unable to start OTA: err %s (err %d)", esp_err_to_name(err), err);
+    goto end1;
+  }
+  if ((err = detools_apply_patch_init(&patch, ota_read, ota_seek, content_len, ota_write, NULL))) {
+    ESP_LOGE("$", "Unable to init OTA: err %d", err);
+    goto end2;
+  }
+
+  while (read_len < content_len) {
+    int len; char buf[512];
+    if ((len = esp_http_client_read(http_handle, buf, sizeof buf)) < 0) {
+      ESP_LOGE("$", "Unable to read HTTP data");
+      goto end1;
+    }
+    read_len += len;
+    ESP_LOGI("$", "Writing OTA chunk (%llu / %llu)...", read_len, content_len);
+    err = detools_apply_patch_process(&patch, (uint8_t *) buf, len);
+    if (err) {
+      ESP_LOGE("$", "Unable to patch OTA chunk: err %d", err);
+      goto end3;
+    }
+  }
+
+end3:
+  if ((err = detools_apply_patch_finalize(&patch)) < 0) {
+    ESP_LOGE("$", "Unable to complete OTA patch: err %d", err);
+  } else if (read_len >= content_len) {
+    ESP_LOGW("$", "OTA patch complete.");
+    if ((err = esp_ota_end(ota_handle)) != ESP_OK) {
+      ESP_LOGE("$", "Unable to complete OTA: %s (err %d)", esp_err_to_name(err), err);
+      goto end1;
+    }
+    if ((err = esp_ota_set_boot_partition(ota_new_partition)) != ESP_OK) {
+      ESP_LOGE("$", "Unable to set OTA boot partition: %s (err %d)", esp_err_to_name(err), err);
+      goto end1;
+    }
+    ESP_LOGW("$", "Rebooting into new firmware...");
+    delay(5000);
+    esp_restart();
+  }
+end2:
+  if ((err = esp_ota_abort(ota_handle)) != ESP_OK)
+    ESP_LOGE("$", "Unable to gracefully abort OTA: err %d", err);
+end1:
+  if ((err = esp_http_client_cleanup(http_handle)) != ESP_OK)
+    ESP_LOGE("$", "Unable to gracefully cleanup HTTP client: %s (err %d)", esp_err_to_name(err), err);
+  }
+}
+
 extern "C" void app_main(){
     initArduino();
     setup();
 
     xTaskCreate(task, "task", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
+    xTaskCreate(ota_task, "ota_task", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
 }
--- firmware/servo/main/main.cpp
+++ firmware/servo/main/main.cpp
@@ -2,6 +2,9 @@
 // - https://gist.github.com/Kaan2626/2728a8ff911c732426403f43383f8bb4
 
 #include <Arduino.h>
+#include <esp_log.h>
+#include <esp_ota_ops.h>
+#include <esp_http_client.h>
 
 #include <freertos/FreeRTOS.h>
 #include <freertos/queue.h>
@@ -11,7 +14,16 @@
 #include "driver/mcpwm.h"
 
 #include <WiFi.h>
-#include <esp_http_client.h>
+
+extern "C" {
+#include <detools.h>
+}
+
+#define CLOUD_HOST "34.241.234.234"
+#define CLOUD_PORT (8000)
+#define CLOUD_ENDPOINT "/servo"
+#define CLOUD_MIN_POLL_MSEC (30000)
+#define CLOUD_MAX_POLL_MSEC (60000)
 
 #define TAG "ESP32resso Servo Control"
 
@@ -20,15 +32,8 @@
 #define TASK_PRIORITY (5)
 
 // WiFi settings
-// TODO(cian): make this more general or read these from another file
-const char* ssid = ":house_with_garden:";
-const char* password = "bluecow6";
-
-// my pc, for now
-// TODO(cian): set this to the EC2 instance's IP address once image uploading is possible
-#define serverAddress "192.168.30.41"
-#define serverPort 8080
-#define serverPath "/servo";
+#define WIFI_SSID "msungie"
+#define WIFI_PSWD "hamandcheese"
 
 // WiFiClient wifiClient;
 // HttpClient httpClient = HttpClient(wifiClient, serverAddress, serverPort);
@@ -117,9 +122,9 @@ int getAngleFromServer() {
   char read_buf[16];
 	int read_len;
 
-  http_config.host = serverAddress;
-	http_config.port = serverPort;
-	http_config.path = serverPath;
+  http_config.host = CLOUD_HOST;
+	http_config.port = CLOUD_PORT;
+	http_config.path = CLOUD_ENDPOINT;
 
   int status_code;
 	if (!(http_handle = esp_http_client_init(&http_config))) {
@@ -129,16 +134,19 @@ int getAngleFromServer() {
 
 	if ((err = esp_http_client_open(http_handle, 0)) != ESP_OK) {
 		ESP_LOGE(TAG, "Unable to open HTTP connection");
+		esp_http_client_cleanup(http_handle);
 		return -1;
 	}
 
 	if ((content_length = esp_http_client_fetch_headers(http_handle)) < 0) {
 		ESP_LOGE(TAG, "Unable to read HTTP headers");
+		esp_http_client_cleanup(http_handle);
 		return -1;
 	}
 
 	if ((status_code = esp_http_client_get_status_code(http_handle)) != 200) {
 		ESP_LOGE(TAG, "Received non-200 status code: %d", status_code);
+		esp_http_client_cleanup(http_handle);
 		return -1;
 	}
 
@@ -168,14 +176,125 @@ void updateServoPosition(void *arg) {
   }
 }
 
+// Occasionally poll cloud for delta-encoded over-the-air firmware updates
+static esp_ota_handle_t ota_handle;
+static const esp_partition_t *ota_new_partition, *ota_old_partition;
+static uintptr_t ota_seek_addr = 0;
+static int ota_read(void *arg, uint8_t *buf, size_t len) { return esp_partition_read(ota_old_partition, ota_seek_addr, buf, len); }
+static int ota_seek(void *arg, int offset) { ota_seek_addr += offset; return 0; }
+static int ota_write(void *arg, const uint8_t *buf, size_t len) { return esp_ota_write(ota_handle, buf, len) || ota_seek(arg, len); }
+static void ota_task(void *arg) {
+  printf("Start of firmware update task\n");
+  uint64_t version = * (uint64_t *) esp_app_get_description()->app_elf_sha256;
+  while (true) {
+    delay(random(CLOUD_MIN_POLL_MSEC, CLOUD_MAX_POLL_MSEC));
+
+  char path[256];
+  esp_err_t err;
+  esp_http_client_config_t http_config = {};
+  esp_http_client_handle_t http_handle;
+  int64_t content_len = 0, read_len = 0;
+  int status_code;
+  struct detools_apply_patch_t patch;
+
+  ESP_LOGD("$", "Querying OTA for version %llX...", version);
+  snprintf(path, sizeof path, "/update?version=servo.%llX", version);
+  http_config.host = CLOUD_HOST;
+  http_config.port = CLOUD_PORT;
+  http_config.path = path;
+  if (!(http_handle = esp_http_client_init(&http_config))) {
+    ESP_LOGE("$", "Unable to create HTTP client");
+    continue;
+  }
+  if ((err = esp_http_client_open(http_handle, 0)) != ESP_OK) {
+    ESP_LOGE("$", "Unable to open HTTP connection: %s (err %d)", esp_err_to_name(err), err);
+    goto end1;
+  }
+  if ((content_len = esp_http_client_fetch_headers(http_handle)) < 0) {
+    ESP_LOGE("$", "Unable to read HTTP headers");
+    goto end1;
+  }
+  if ((status_code = esp_http_client_get_status_code(http_handle)) != 200) {
+    ESP_LOGE("$", "Received non-200 status code: %d", status_code);
+    goto end1;
+  }
+  if (!content_len) {
+    ESP_LOGD("$", "No OTA for version %llX received", version);
+    goto end1;
+  }
+
+  ESP_LOGI("$", "Updating firmware (%lld byte patch)...", content_len);
+
+  ota_seek_addr = 0;
+  if (!(ota_new_partition = esp_ota_get_next_update_partition(NULL))) {
+    ESP_LOGE("$", "Unable to find valid OTA partition to use");
+    goto end1;
+  }
+  if (!(ota_old_partition = esp_ota_get_running_partition())) {
+    ESP_LOGE("$", "Unable to find current OTA partition in use");
+    goto end1;
+  }
+  err = esp_ota_begin(ota_new_partition, OTA_SIZE_UNKNOWN, &ota_handle);
+  if (err != ESP_OK) {
+    ESP_LOGE("$", "Unable to start OTA: err %s (err %d)", esp_err_to_name(err), err);
+    goto end1;
+  }
+  if ((err = detools_apply_patch_init(&patch, ota_read, ota_seek, content_len, ota_write, NULL))) {
+    ESP_LOGE("$", "Unable to init OTA: err %d", err);
+    goto end2;
+  }
+
+  while (read_len < content_len) {
+    int len; char buf[512];
+    if ((len = esp_http_client_read(http_handle, buf, sizeof buf)) < 0) {
+      ESP_LOGE("$", "Unable to read HTTP data");
+      goto end1;
+    }
+    read_len += len;
+    ESP_LOGI("$", "Writing OTA chunk (%llu / %llu)...", read_len, content_len);
+    err = detools_apply_patch_process(&patch, (uint8_t *) buf, len);
+    if (err) {
+      ESP_LOGE("$", "Unable to patch OTA chunk: err %d", err);
+      goto end3;
+    }
+  }
+
+end3:
+  if ((err = detools_apply_patch_finalize(&patch)) < 0) {
+    ESP_LOGE("$", "Unable to complete OTA patch: err %d", err);
+  } else if (read_len >= content_len) {
+    ESP_LOGW("$", "OTA patch complete.");
+    if ((err = esp_ota_end(ota_handle)) != ESP_OK) {
+      ESP_LOGE("$", "Unable to complete OTA: %s (err %d)", esp_err_to_name(err), err);
+      goto end1;
+    }
+    if ((err = esp_ota_set_boot_partition(ota_new_partition)) != ESP_OK) {
+      ESP_LOGE("$", "Unable to set OTA boot partition: %s (err %d)", esp_err_to_name(err), err);
+      goto end1;
+    }
+    ESP_LOGW("$", "Rebooting into new firmware...");
+    delay(5000);
+    esp_restart();
+  }
+end2:
+  if ((err = esp_ota_abort(ota_handle)) != ESP_OK)
+    ESP_LOGE("$", "Unable to gracefully abort OTA: err %d", err);
+end1:
+  if ((err = esp_http_client_cleanup(http_handle)) != ESP_OK)
+    ESP_LOGE("$", "Unable to gracefully cleanup HTTP client: %s (err %d)", esp_err_to_name(err), err);
+  }
+}
+
 extern "C" void app_main(){
     printf("Started app_main\n");
 
     initArduino();
-    WiFi.begin(ssid, password);
-    while ( WiFi.status() != WL_CONNECTED) {
-      Serial.print(".");
-      delay(100); // wait 100ms
+    WiFi.begin(WIFI_SSID, WIFI_PSWD);
+    for (int i = 0; WiFi.status() != WL_CONNECTED && i < 30; i++)
+        delay(1000);
+    if (WiFi.status() != WL_CONNECTED) {
+        ESP_LOGE("$MAIN", "Failed to connect to WiFi, rebooting...");
+        esp_restart();
     }
 
     // xTaskCreate Parameters
@@ -188,4 +307,5 @@ extern "C" void app_main(){
     // TaskHandle_t *pxCreatedTask
     xTaskCreate(moveServo, "moveServo", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
     xTaskCreate(updateServoPosition, "updateServoPosition", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
+    xTaskCreate(ota_task, "ota_task", TASK_STACK_SIZE, NULL, TASK_PRIORITY, NULL);
   }
